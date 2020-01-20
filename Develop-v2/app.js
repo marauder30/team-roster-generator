aed751c13eccfa4ae1e2255533ec5a4cdca04a7c
Nick Koch <marauder30@gmail.com>
2020-01-20 11:29:00 -0700

    wrote some psuedo, switched the order on some questions

--- Develop-v2/app.js
+++ Develop-v2/app.js
@@ -10,37 +10,100 @@ const outputPath = path.resolve(__dirname, "output", "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const managers = [];
+const engineers = [];
+const interns = [];
+
+// try a switch case 
+
+// create arrays for each employee type
+
+// push object to array for each type, or return for thats all
+
 inquirer.prompt([
-    {
-        type: 'input',
-        name: 'Employee Name',
-        message: "What is the employee's name?",
-    },
-    {
-        type: 'input',
-        name: 'Employee ID',
-        message: "What is their company ID number?",
-        validate: function(val) {
-            var valid = !isNaN(parseFloat(val));
-            return valid || 'Please enter a number';
-        },
-        filter: Number
-    },
     {
         type: 'list',
         name: 'Employee Role',
         message: "What is their role in the company?",
-        choices: ['Engineer', 'Intern', 'Manager']
-    }
+        choices: ['Engineer', 'Intern', 'Manager', "No more employees"]
+    },
 ]).then(answers => {
+    
 
-    if (answers['Employee Role'] === 'Engineer') {
-
-        console.log("true");
+    if(answers['Employee Role'] === "No more employees") {
+        return;
     }
-
-
+    const role = answers["Employee Role"];
+    console.log(role);
+    
+    if (answers['Employee Role'] === 'Engineer') {
+        
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'GitHub Username',
+                message: "What is their GitHub username?"
+            }
+        ]).then(answers => {
+            
+            const github = answers["GitHub Username"];
+            console.log(github);
+        })
+    }
+    if (answers["Employee Role"] === 'Intern') {
+        
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: "School",
+                message: "What school is the internship through?"
+            }
+        ]).then(answers => {
+            
+            const school = answers['School'];
+            console.log(school);
+        })
+    }
+    if (answers["Employee Role"] === 'Manager') {
+        
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: "Office Number",
+                message: "What is the manager's office number?",
+                validate: function(val) {
+                    var valid = !isNaN(parseFloat(val));
+                    return valid || 'Please enter a number';
+                },
+                filter: Number
+            }
+        ]).then(answers => {
+            
+            const officeNumber = answers["Office Number"];
+            console.log(officeNumber);
+            console.log(role);
+        })
+    }
+    
+    
 })
+
+// {
+//     type: 'input',
+//     name: 'Employee Name',
+//     message: "What is the employee's name?",
+// },
+// {
+//     type: 'input',
+//     name: 'Employee ID',
+//     message: "What is their company ID number?",
+//     validate: function(val) {
+//         var valid = !isNaN(parseFloat(val));
+//         return valid || 'Please enter a number';
+//     },
+//     filter: Number
+// }
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
